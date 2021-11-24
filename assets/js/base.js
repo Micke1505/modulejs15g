9c28a92a09f8fa099512c02b3294f530662d18c5
Micke1505 <mickeangel27@gmail.com>
2021-11-23 21:28:39 -0700

    Exercise number one, class 1 Javascript

--- assets/js/base.js
+++ assets/js/base.js
@@ -1,1 +1,41 @@
-console.log('Hola Mundo')
+/*
+Ejercicio1: 
+pedir 2 variables por prompt
+Si a> b dividir a entre b
+Si a < b sumar a mas b
+Si a == b multiplicar ambos numeros 
+Imprimir el resultado en consola
+
+ My solution
+var a;
+var b;
+
+
+a=prompt( "What is your first variable");
+b=prompt( "What is your second variable");
+
+var x =parseInt(a);
+var y =parseInt(b);
+
+if(x > y){
+    console.log (a/b);
+};
+
+if(x < y){
+    console.log (a+b);
+};
+
+if(x = y){
+    console.log (a*b);
+}; 
+Solution in class*/
+const a = parseInt(prompt('What is the first value??'));
+const b = parseInt(prompt('What is the second value??'));
+
+if( a > b){
+    console.log (a/b)
+}else if (a < b){
+    console.log (a + b)
+}else {
+    console.log (a*b)
+}
